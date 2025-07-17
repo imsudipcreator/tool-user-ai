a10e70d449dac2c9b282abec86f9bb5d99db103b
imsudipcreator <imsudipmahata@gmail.com>
2025-07-17 19:09:34 +0530

    advanced_coder tool added, system prompt improved

--- app/tools/__init__.py
+++ app/tools/__init__.py
@@ -3,5 +3,6 @@ from .current_time import current_time
 from .wiki import wiki_search
 from .img_to_img_gen import generate_image_from_image
 from .imago_info import get_imago_info
+from .advanced_coder import advanced_coder
 
-__all__ = ["generate_image", "current_time", "wiki_search", "generate_image_from_image", "get_imago_info"]
+__all__ = ["generate_image", "current_time", "wiki_search", "generate_image_from_image", "get_imago_info", "advanced_coder"]
