d2945030d05400a38b8df4e878ee4c26847bd5b8
imsudipcreator <imsudipmahata@gmail.com>
2025-08-24 11:10:18 +0530

    updated code gen agent

--- app/agents/code_gen_agent.py
+++ app/agents/code_gen_agent.py
@@ -10,13 +10,24 @@ def code_agent_response(input: CodeAgentInput):
     try:
         client = Client("akhaliq/anycoder")
         result = client.predict(
-            query=input.prompt,
-            image=None,
-            file=None,
-            website_url="",
-            enable_search=False,
-            language="html",
-            api_name="/generation_code",
+            query=f"{input.prompt} and write html, css, javascript in a single html file. Don't add anything like '=== index.html ===' or anything similar. if you want you can add comment only <!-- index.html -->",
+                vlm_image=handle_file('https://raw.githubusercontent.com/gradio-app/gradio/main/test/test_files/bus.png'),
+                gen_image=handle_file('https://raw.githubusercontent.com/gradio-app/gradio/main/test/test_files/bus.png'),
+                file=handle_file('https://github.com/gradio-app/gradio/raw/main/test/test_files/sample_file.pdf'),
+                website_url="",
+                enable_search=False,
+                language="html",
+                enable_image_generation=False,
+                enable_image_to_image=False,
+                image_to_image_prompt=None,
+                text_to_image_prompt=None,
+                enable_image_to_video=False,
+                image_to_video_prompt=None,
+                enable_text_to_video=False,
+                text_to_video_prompt=None,
+                enable_text_to_music=False,
+                text_to_music_prompt=None,
+                api_name="/generation_code"
         )
 
         return {"response": result[0], "error": None}
